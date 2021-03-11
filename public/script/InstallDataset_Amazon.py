3dd3e20c20908867d2396226b797e4ffa5c86691
xiongkun <xk18@mails.tsinghua.edu.cn>
2021-03-12 01:49:35 +0800

    xk

--- public/script/InstallDataset_Amazon.py
+++ public/script/InstallDataset_Amazon.py
@@ -5,11 +5,10 @@ os.system("mkdir -p /home/data/dataset")
 os.system("mkdir -p /home/data/cache")
 if not osp.exists(osp.join('/home/data/cache', 'Amazon.tar')):
     print ("Downloading Amazon dataset for cvpack_rec")
-    os.system('cd /home/data/cache && wget -c https://cloud.tsinghua.edu.cn/f/38cf90e4c5984b388ef5/?dl=1')
+    os.system('wget -c https://cloud.tsinghua.edu.cn/f/38cf90e4c5984b388ef5/?dl=1 -O /home/data/cache/Amazon.tar')
     print ("Downloaded")
 os.system('tar -xf /home/data/cache/Amazon.tar -C /home/data/cache/')
 os.system('mv /home/data/cache/Amazon /home/data/dataset')
 
 print ("Process Complete!!")
-os.system('rm -r /home/data/cache')
-
+# os.system('rm -r /home/data/cache')
